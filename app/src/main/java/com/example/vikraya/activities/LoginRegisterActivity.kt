deea03bacb5bfd5da40c78f4be4b6f3be91ef246
Sarthak Shukla <sarthakshukla500@gmail.com>
2024-04-08 14:14:11 +0530

    3rd Commit

--- app/src/main/java/com/example/vikraya/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/vikraya/activities/LoginRegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.example.vikraya
+package com.example.vikraya.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vikraya.R
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_register)
     }
 }
---
+++ app/src/main/java/com/example/vikraya/activities/ShopingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vikraya.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vikraya.R
+
+class ShopingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoping)
+    }
+}
